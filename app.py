8bd7b80d865e2badd1dbf29ffbabea1d8adcc3eb
Alexandre-SDV <gouraudalex35@gmail.com>
2025-04-27 15:59:58 +0200

    Correction App run
    Correction App run

--- app.py
+++ app.py
@@ -75,4 +75,6 @@ def index():
     return render_template('index.html', uploaded_file=uploaded_file, filtered_image=filtered_image)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(debug=True)  # Pour le développement, pas nécessaire en production
+
